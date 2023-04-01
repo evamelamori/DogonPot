e23baf0a561f84a07d7c5d821ea5d302c04f7e5b
evamelamori <evamelamori@gmail.com>
2023-03-31 21:52:39 -0500

    Add project files.

---
+++ DogonPot/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * //TODO: Evelina Hoidenko
+ * Recursion Assignment
+ * //TODO: 19/02/2023
+ * Based on Dogon Pot pseudo code from the book
+ * African Fractals: Modern Computing and Indigenous Design by Ron Eglash
+ */
+
+using System;
+
+namespace DogonPotVariation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Example steps to draw a Dogon-like pot.";
+            new Pot();
+            Console.ReadKey();
+
+        }
+    }
+}
